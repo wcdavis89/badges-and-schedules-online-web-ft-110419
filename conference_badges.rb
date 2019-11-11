13afe1542e5efc51853d7785fd45b3f9b12d2997
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 15:29:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,13 @@ def assign_rooms(array)
   p assignment
 end
 
-def printer greet=(batch_badge_creator(array))
- room=(assign_rooms(array))
-
-
+def printer(attendees)
+  resultOne = batch_badge_creator(attendees)
+  resultOne.each do |x|
+    puts x 
+  end
+  result = assign_rooms(attendees)
+  result.each do |x|
+    puts x 
+  end
 end
