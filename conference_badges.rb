8022e8c4af066fa910fa9e715ba3e6be4c53a724
Whittney Davis <wcdavis89@yahoo.com>
2019-11-11 12:50:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+ p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  message=[]
+  array.each do |attendee|  
+ message.push(badge_maker(attendee))
+end  
+p message
+end
+
+def assign_rooms(array)
+  assignment=[] 
+  array.each_with_index do |attendee,room|
+    assignment.push("Hello, #{attendee}! You'll be assigned to room #{room+=1}!")
+  end
+  p assignment
+end
+
+def printer(greet,assignment)
+   p batch_badge_creator(array)
+  p assign_rooms(array)
+end
